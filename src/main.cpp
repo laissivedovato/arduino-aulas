f9c1419f27a0067a4efa8c0e5b77f01461674544
LaissiVedovato <laissi.vedovato@gmail.com>
2024-01-02 16:51:15 -0300

    Motor DC - Aula 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,12 @@
-//FUNÇÃO CONSTRAIN -> determinar uma faixa de valor
-//constrain(valor_lido, min_saida, max-saida);
-//constrain(valor_lido_pot, 500, 800)
+//MOTOR DC
 #include <Arduino.h>
-#define pot A0
+#define motor 2
 
-void setup( ){
-  pinMode(pot,INPUT);
-  Serial.begin(9600);
+void setup() {
+  pinMode(motor, OUTPUT);
+  digitalWrite(motor, HIGH);
 }
 
 void loop() {
-  Serial.println(constrain(analogRead(pot),500, 800));
-  delay(250);
+
 }
