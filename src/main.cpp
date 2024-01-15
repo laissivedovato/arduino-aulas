78a734462272cfa0268fc76d2e7aca4024f228c0
LaissiVedovato <laissi.vedovato@gmail.com>
2024-01-15 17:22:06 -0300

    Motor Dc - Potenciometro

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
-//MOTOR DC - diodo-  AULA 2
+//MOTOR DC - DIODO 3
 #include <Arduino.h>
-#define motor 2
-int time= 2000;
+#define pot A0
+#define motor 3
+#define time 10
+int valpot;
 
 void setup() {
   pinMode(motor, OUTPUT);
+  pinMode(pot,INPUT);
+  valpot=0;
 }
 
 void loop() {
-  digitalWrite(motor, HIGH);
-  delay(time);
-  digitalWrite(motor, LOW);
+  valpot=map(analogRead(pot), 0, 1023, 0, 255);
+  analogWrite(motor, valpot);
   delay(time);
 }
